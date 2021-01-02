d7b6c5a0c0d58b862b246d5e6ab297ab6025c1ea
ramez <ramezmagdy120@gmail.com>
2021-01-02 20:14:41 +0200

    final version

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,21 +1,64 @@
+import React, { useRef } from "react";
 import { useEffect } from "react";
-
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import { Container } from "react-bootstrap";
 
 const StreamShow = (props) => {
   const { id } = props.match.params;
-  const  fetchStream  = props.fetchStream;
+  const fetchStream = props.fetchStream;
+  let videoRef = useRef();
+  let player;
+
+  // check if no stream is available or player already running
+  const buildPlayer = (id) => {
+    if (!props.stream || player) {
+      return;
+    }
+     // create player
+    player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  };
+
+
+  // fetch user id
   useEffect(() => {
     fetchStream(id);
-  }, [id, fetchStream]);
+  }, [id]);
+
+  // check after fetching id to build player
+  useEffect(() => {
+    buildPlayer(id);
+
+    // clean player up if exists
+    return (() =>{
+      if(player)
+      player.destroy();
+    })
+  }, [props.stream]);
+
   if (!props.stream) {
-    return <div>Loading ..</div>;
+    return (
+     <Container>
+    <div>Loading ...</div>
+    </Container>)
   }
   return (
     <div>
+      <Container>
+    <div >
+      <video className = "w-100" ref={videoRef} controls></video>
+      </div>
+      <div>
       <h2>{props.stream.title}</h2>
       <p>{props.stream.description}</p>
+      </div>
+      </Container>
     </div>
   );
 };
